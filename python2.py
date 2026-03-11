91fd0c8a7f352056545e48e70b01fab172b9f6b1
Keerthana m <keerthanam355@gmail.com>
2026-03-11 19:24:55 +0530

    Add files via upload
    i have did a small program to understand decision making statements in python.

---
+++ python2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter 1 num:"))
+num2 = int(input("Enter 2 num"))
+if (num1 > 10 and num2 > 10):
+    print("both are greater than 10")
+elif (num1 < 5 or num2 < 5 ):
+    print("atleast one number is less than 5")
+if  (num1 <= num2 ):
+    print("1 is not greater than 2")
+    
